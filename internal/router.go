9a7aca4ebca3aac85b0dfba0620e7bfdc1035f7c
Kirill <kirill.l.2004@yandex.ru>
2024-10-27 23:51:01 +0300

    upload image

--- internal/router.go
+++ internal/router.go
@@ -11,6 +11,7 @@ func SetupRouter() (*mux.Router) {
 
 	router.HandleFunc("/api/mesh", api.SaveMeshObjectHandler).Methods("POST")
 	router.HandleFunc("/api/mesh/{id:[0-9]+}", api.GetMeshObjectHandler).Methods("GET")
+	router.HandleFunc("/api/upload", api.UploadImage).Methods("POST")
 	router.HandleFunc("/api/run-script", api.RunScript).Methods("POST")
 
 	return router
